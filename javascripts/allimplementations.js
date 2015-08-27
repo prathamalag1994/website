b67ebdb19c4b60324d88a5fc1864dedc2cacfcdf
prathamalag1994 <prathamalag@berkeley.edu>
2015-08-26 17:41:13 -0700

    switched the website: no time to remake

--- javascripts/allimplementations.js
+++ javascripts/allimplementations.js
@@ -28,4 +28,25 @@ $(document).ready(function(){
     $("#contact").css("-webkit-transform","scale(0.91)");
   });
 
+
+   $("#resumee").hover(function(){
+    $("#hide").hide();
+  });
+
+  $("#resumee").click(function(){
+    $("#resumee").text(":)");
+  });
+
+  $(".menu-trigger").hover(function(){
+    $("#hide").show();
+  });
+
+   $("#resumee").mouseover(function(){
+    $("#resumee").css("-webkit-transform","scale(1.1)");
+  });
+
+   $("#resumee").mouseout(function(){
+    $("#resumee").css("-webkit-transform","scale(0.91)");
+  });
+
 });
